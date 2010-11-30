f0db5d281787bf9b93ffb9923d2571db075de2a2
JeanMichel FRANCOIS aka toutpt <toutpt@gmail.com>
2010-11-30 23:03:30 +0100

    refactor libraries

--- collective/googlelibraries/libraries.py
+++ collective/googlelibraries/libraries.py
@@ -13,19 +13,26 @@ from collective.googlelibraries import messageFactory as _
 
 class Library(object):
     interface.implements(interfaces.ILibrary)
-    def __init__(self, id, title, version, url_u, url):
+    def __init__(self, id, title, url_u, url, versions, version=''):
         self.id = id
         self.title = title
+        self.versions = versions
         self.version = version
-        self._url_u = url_u
         self._url = url
-        self.mode = 'minified'
+        self._url_u = url_u
+        self.minified = True
 
     @property
     def url(self):
-        if self.mode != 'minified':
-            return self._url_u
-        return self._url
+        base = self._url
+        if not self.minified:
+            base = self._url_u
+
+        version = self.version
+        if not self.version:
+            version = self.versions[-1]
+
+        return base%({'version':version})
 
     def __str__(self):
         return '%s | %s'%(self.id, self.version)
@@ -34,80 +41,70 @@ class Library(object):
 
 GOOGLE_LIBRARIES = {}
 
-GOOGLE_LIBRARIES["chrome-frame"] = {}
-for v in ('1.0.0', '1.0.1', '1.0.2'):
-    url = "https://ajax.googleapis.com/ajax/libs/chrome-frame/%s/CFInstall.min.js"%v
-    url_u = "https://ajax.googleapis.com/ajax/libs/chrome-frame/%s/CFInstall.js"%v
-    GOOGLE_LIBRARIES["chrome-frame"][v] = Library("chrome-frame","Chrome Frame", v, url_u, url)
+v = ('1.0.0', '1.0.1', '1.0.2')
+url = "https://ajax.googleapis.com/ajax/libs/chrome-frame/%(version)s/CFInstall.min.js"
+url_u = "https://ajax.googleapis.com/ajax/libs/chrome-frame/%(version)s/CFInstall.js"
+lib = Library("chrome-frame", "Chrome Frame", v, url_u, url)
+GOOGLE_LIBRARIES["chrome-frame"] = lib
 
-GOOGLE_LIBRARIES["dojo"] = {}
-for v in ('1.1.1',
+v = ('1.1.1',
           '1.2.0', '1.2.3',
           '1.3.0', '1.3.1', '1.3.2',
           '1.4.0', '1.4.1', '1.4.3',
-          '1.5'):
-    url_u = "https://ajax.googleapis.com/ajax/libs/dojo/%s/dojo/dojo.xd.js.uncompressed.js"%v
-    url = "https://ajax.googleapis.com/ajax/libs/dojo/%s/dojo/dojo.xd.js"%v
-    GOOGLE_LIBRARIES["dojo"][v] = Library("dojo","Dojo", v, url_u, url)
+          '1.5')
+url_u = "https://ajax.googleapis.com/ajax/libs/dojo/%s/dojo/dojo.xd.js.uncompressed.js"
+url = "https://ajax.googleapis.com/ajax/libs/dojo/%s/dojo/dojo.xd.js"
+lib = Library("dojo","Dojo", v, url_u, url)
+GOOGLE_LIBRARIES["dojo"] = lib
 
-GOOGLE_LIBRARIES["ext-core"] = {}
-for v in ('3.0.0','3.1.0'):
-    url_u = "https://ajax.googleapis.com/ajax/libs/ext-core/%s/ext-core-debug.js"%v
-    url = "https://ajax.googleapis.com/ajax/libs/ext-core/%s/ext-core.js"%v
-    GOOGLE_LIBRARIES["ext-core"][v] = Library("ext-core","Ext Core", v, url_u, url)
+v = ('3.0.0','3.1.0')
+url_u = "https://ajax.googleapis.com/ajax/libs/ext-core/%s/ext-core-debug.js"
+url = "https://ajax.googleapis.com/ajax/libs/ext-core/%s/ext-core.js"
+lib = Library("ext-core","Ext Core", v, url_u, url)
 
-GOOGLE_LIBRARIES["jquery"] = {}
-for v in ('1.2.3', '1.2.6',
-          '1.3.0', '1.3.1', '1.3.2',
-          '1.4.0', '1.4.1', '1.4.2','1.4.3','1.4.4'):
-    url_u = "https://ajax.googleapis.com/ajax/libs/jquery/%s/jquery.js"%v
-    url = "https://ajax.googleapis.com/ajax/libs/jquery/%s/jquery.min.js"%v
-    GOOGLE_LIBRARIES["jquery"][v] = Library("jquery","jQuery", v, url_u, url)
+GOOGLE_LIBRARIES["ext-core"] = lib
 
-GOOGLE_LIBRARIES["jqueryui"] = {}
-for v in ('1.5.2', '1.5.3',
-          '1.6.0',
-          '1.7.0','1.7.1', '1.7.2', '1.7.3',
-          '1.8.0', '1.8.1', '1.8.2', '1.8.4', '1.8.5','1.8.6'):
-    url_u = "https://ajax.googleapis.com/ajax/libs/jqueryui/%s/jquery-ui.js"%v
-    url = "https://ajax.googleapis.com/ajax/libs/jqueryui/%s/jquery-ui.min.js"%v
-    GOOGLE_LIBRARIES["jqueryui"][v] = Library("jqueryui","jQuery UI", v, url_u, url)
+v = ('1.2.3', '1.2.6',
+     '1.3.0', '1.3.1', '1.3.2',
+     '1.4.0', '1.4.1', '1.4.2','1.4.3','1.4.4')
+url_u = "https://ajax.googleapis.com/ajax/libs/jquery/%s/jquery.js"
+url = "https://ajax.googleapis.com/ajax/libs/jquery/%s/jquery.min.js"
+GOOGLE_LIBRARIES["jquery"] = Library("jquery","jQuery", v, url_u, url)
 
-GOOGLE_LIBRARIES["mootools"] = {}
-for v in ('1.1.1', '1.1.2', '1.2.1', '1.2.2', '1.2.3', '1.2.4', '1.2.5','1.3.0'):
-    url_u = "https://ajax.googleapis.com/ajax/libs/mootools/%s/mootools.js"%v
-    url = "https://ajax.googleapis.com/ajax/libs/mootools/%s/mootools-yui-compressed.js"%v
-    GOOGLE_LIBRARIES["mootools"][v] =  Library("mootools","MooTools", v, url_u, url)
+v = ('1.5.2', '1.5.3',
+     '1.6.0',
+     '1.7.0','1.7.1', '1.7.2', '1.7.3',
+     '1.8.0', '1.8.1', '1.8.2', '1.8.4', '1.8.5','1.8.6')
+url_u = "https://ajax.googleapis.com/ajax/libs/jqueryui/%s/jquery-ui.js"
+url = "https://ajax.googleapis.com/ajax/libs/jqueryui/%s/jquery-ui.min.js"
+GOOGLE_LIBRARIES["jqueryui"] = Library("jqueryui","jQuery UI", v, url_u, url)
 
-GOOGLE_LIBRARIES["prototype"] = {}
-for v in ('1.6.0.2', '1.6.0.3', '1.6.1.0','1.7.0.0'):
-    url_u = "https://ajax.googleapis.com/ajax/libs/prototype/%s/prototype.js"%v
-    url = "https://ajax.googleapis.com/ajax/libs/prototype/%s/prototype.js"%v
-    GOOGLE_LIBRARIES["prototype"][v] = Library("prototype","Prototype", v, url_u, url)
+v = ('1.1.1', '1.1.2', '1.2.1', '1.2.2', '1.2.3', '1.2.4', '1.2.5','1.3.0')
+url_u = "https://ajax.googleapis.com/ajax/libs/mootools/%s/mootools.js"
+url = "https://ajax.googleapis.com/ajax/libs/mootools/%s/mootools-yui-compressed.js"
+GOOGLE_LIBRARIES["mootools"] =  Library("mootools","MooTools", v, url_u, url)
 
-GOOGLE_LIBRARIES["scriptaculous"] = {}
-for v in ('1.8.1', '1.8.2','1.8.3'):
-    url_u = "https://ajax.googleapis.com/ajax/libs/scriptaculous/%s/scriptaculous.js"%v
-    url = "https://ajax.googleapis.com/ajax/libs/scriptaculous/%s/scriptaculous.js"%v
-    GOOGLE_LIBRARIES["scriptaculous"][v] = Library("scriptaculous","script.aculo.us", v, url_u, url)
+v = ('1.6.0.2', '1.6.0.3', '1.6.1.0','1.7.0.0')
+url_u = "https://ajax.googleapis.com/ajax/libs/prototype/%s/prototype.js"
+url = "https://ajax.googleapis.com/ajax/libs/prototype/%s/prototype.js"
+GOOGLE_LIBRARIES["prototype"] = Library("prototype","Prototype", v, url_u, url)
 
-GOOGLE_LIBRARIES["swfobject"] = {}
-for v in ('2.1','2.2'):
-    url_u = "https://ajax.googleapis.com/ajax/libs/swfobject/%s/swfobject_src.js"%v
-    url = "https://ajax.googleapis.com/ajax/libs/swfobject/%s/swfobject.js"%v
-    GOOGLE_LIBRARIES["swfobject"][v] = Library("swfobject","SWFObject", v, url_u, url)
+v = ('1.8.1', '1.8.2','1.8.3')
+url_u = "https://ajax.googleapis.com/ajax/libs/scriptaculous/%s/scriptaculous.js"
+url = "https://ajax.googleapis.com/ajax/libs/scriptaculous/%s/scriptaculous.js"
+lib = Library("scriptaculous","script.aculo.us", v, url_u, url)
+GOOGLE_LIBRARIES["scriptaculous"] = lib
 
-GOOGLE_LIBRARIES["yui"] = {}
-for v in ('2.6.0', '2.7.0', '2.8.0r4', '2.8.1','2.8.2'):
-    url_u = "https://ajax.googleapis.com/ajax/libs/yui/%s/build/yuiloader/yuiloader.js"%v
-    url = "https://ajax.googleapis.com/ajax/libs/yui/%s/build/yuiloader/yuiloader-min.js"%v
-    GOOGLE_LIBRARIES["yui"][v] = Library("yui","Yahoo! User Interface Library (YUI)", v, url_u, url)
+v = ('2.1','2.2')
+url_u = "https://ajax.googleapis.com/ajax/libs/swfobject/%s/swfobject_src.js"
+url = "https://ajax.googleapis.com/ajax/libs/swfobject/%s/swfobject.js"
+GOOGLE_LIBRARIES["swfobject"] = Library("swfobject","SWFObject", v, url_u, url)
 
-terms = []
-for l in GOOGLE_LIBRARIES.keys():
-    terms.append(schema.vocabulary.SimpleTerm(l,l,l))
-
-GOOGLE_LIBRARIES_VOCABULARY = schema.vocabulary.SimpleVocabulary(terms)
+v = ('2.6.0', '2.7.0', '2.8.0r4', '2.8.1','2.8.2')
+url_u = "https://ajax.googleapis.com/ajax/libs/yui/%s/build/yuiloader/yuiloader.js"
+url = "https://ajax.googleapis.com/ajax/libs/yui/%s/build/yuiloader/yuiloader-min.js"
+lib = Library("yui","Yahoo! User Interface Library (YUI)", v, url_u, url)
+GOOGLE_LIBRARIES["yui"] = lib
 
 
 class LibraryWidget(ASCIIWidget):
@@ -174,9 +171,23 @@ class LibraryManager(SchemaAdapterBase):
     loader_mode = property(get_loader_mode, set_loader_mode)
 
     def get_libraries(self):
-        return getattr(self.properties, config.PROPERTY_LIBRARIES_FIELD, '')
+        res = []
+        conf = getattr(self.properties, config.PROPERTY_LIBRARIES_FIELD, '')
+        self.context.plone_log('get ' + str(conf))
+
+        for lib in conf:
+            value = lib.split('|')
+            libname = value[0].strip()
+            library = GOOGLE_LIBRARIES[libname]
+            version = value[1].strip()
+            if version:
+                library.version = version
+            res.append(library)
+        return conf
+#        return tuple(res)
 
     def set_libraries(self, value):
+        self.context.plone_log('set ' + str(value))
         self.properties._updateProperty(config.PROPERTY_LIBRARIES_FIELD, value)
 
     libraries = property(get_libraries, set_libraries)
@@ -192,12 +203,10 @@ class LibraryManager(SchemaAdapterBase):
         for lib in self.libraries:
             value = lib.split('|')
             libname = value[0].strip()
+            library = GOOGLE_LIBRARIES[libname]
             version = value[1].strip()
-            if not version:
-                #get the last available one
-                versions = GOOGLE_LIBRARIES[libname].keys()
-                versions.sort()
-                version = versions[-1]
-            res[libname] = GOOGLE_LIBRARIES[libname][version]
+            if version:
+                library.version = version
+            res[libname] = library
 
         return res
